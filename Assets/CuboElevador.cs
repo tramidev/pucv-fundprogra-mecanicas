05edb687bfa44508d609ce4ca37defcbc4adc443
tramidev <tomas@abstractdw.com>
2025-06-18 16:59:13 -0400

    step01-nobugs

--- Assets/CuboElevador.cs
+++ Assets/CuboElevador.cs
@@ -3,29 +3,54 @@ using UnityEngine;
 public class CuboElevador : MonoBehaviour
 {
     public GameObject texto;
+    public Vector3 puntoFinal;
+    private bool puertaAbierta;
+    private bool playerInteractuando;
 
     private void Start()
     {
-        texto.SetActive(false);
+        PrenderTexto(false);
     }
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.F))
+        if (Input.GetKeyUp(KeyCode.F) && playerInteractuando)
         {
             Debug.Log("Usuario Presionó tecla F");
+            
+            AbrirPuerta();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        texto.SetActive(true);
+        PrenderTexto(false);
         Debug.Log("Mensaje Entrada");
+        playerInteractuando = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("Mensaje Salida");
-        texto.SetActive(false);
+        PrenderTexto(false);
+        playerInteractuando = false;
+    }
+
+    void AbrirPuerta()
+    {
+        if (!puertaAbierta)
+        {
+            texto.SetActive(false);
+            puertaAbierta = true;
+            transform.position = transform.position + puntoFinal;
+        }
+    }
+
+    void PrenderTexto(bool prender)
+    {
+        if (!puertaAbierta)
+        {
+            texto.SetActive(prender);
+        }
     }
 }
